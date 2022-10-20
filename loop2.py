f24a5160d25964945b16d60a4e8a9b9a2b046ce7
httpstephanie <110868588+httpstephanie@users.noreply.github.com>
2022-10-20 13:21:01 -0500

    Add files via upload

---
+++ loop2.py
@@ -0,0 +1,7 @@
+# realiza la suma de todos los números utilizando loops For y almacena el resultado de la suma en una variable
+lista_numeros = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+ 
+suma_numeros = 0
+ 
+for numero in lista_numeros:
+    suma_numeros = suma_numeros + numero
